773dbe890fa290b37b553e07d0d73fee92134bc2
1V4NJ22 <58572213+1V4NJ22@users.noreply.github.com>
2022-04-14 23:30:43 -0700

    Boton arranque y arreglado dientes de sierra

--- Camara_giratoria.ino
+++ Camara_giratoria.ino
@@ -1,8 +1,10 @@
 //Entradas
-const byte potenciometro = A0;
+//Entrada analógica entre 0 y 1023 (10 bits)
+const int potenciometro = A0;
 //Salidas
 const byte motor = 5;
 const byte rele = 3;
+const byte arranque = 2;
 //Constantes
 
 //Variables
@@ -10,33 +12,31 @@ byte velocidad;
 //  señal recibida por bluetooth
  
 void setup(){
+  //Serial.begin(9600);
   pinMode(potenciometro, INPUT);
+  pinMode(arranque, INPUT_PULLUP);
   pinMode(motor, OUTPUT);
   pinMode(rele, OUTPUT);
 }
 void loop(){
-  digitalWrite(rele, HIGH);
-  delay(1000);
-  velocidad = analogRead(potenciometro);
-  digitalWrite(rele, LOW);
-  while (true)
+  //Arranque manual del motor
+  if (digitalRead(arranque))
   {
-    if (velocidad > 255*0.98)
+    while (digitalRead(arranque))
     {
-      velocidad = 255*0.98;
+      digitalWrite(rele, HIGH);
+      delay(100);
     }
-    analogWrite(motor, velocidad);
-    if (velocidad < 26)
-    {
-      while (velocidad < 26)
-      {
-        velocidad = analogRead(potenciometro);
-	analogWrite(motor, velocidad);
-        delay(30);
-      }
-      break;
-    }
-    delay(500);
-    velocidad = analogRead(potenciometro);
   }
+  
+  //Se divide entre 4 porque el pwm es de 8 bits (256)
+  velocidad = analogRead(potenciometro)/4;
+  digitalWrite(rele, LOW);
+  if (velocidad > 255*0.98)
+  {
+    velocidad = 255*0.98;
+  }
+  analogWrite(motor, velocidad);
+  delay(50);
+  velocidad = analogRead(potenciometro);
 }
