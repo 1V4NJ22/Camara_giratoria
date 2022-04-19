a4658896a9bb93e0ef807206c5f241903ebcd4a5
1V4NJ22 <58572213+1V4NJ22@users.noreply.github.com>
2022-04-18 23:53:09 -0700

    Con bluetooth

--- Camara_giratoria.ino
+++ Camara_giratoria.ino
@@ -1,27 +1,76 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(A4, A5); //Rx,
 //Entradas
-//Entrada analógica entre 0 y 1023 (10 bits)
-const int potenciometro = A0;
+
 //Salidas
-const byte motor = 5;
-const byte encendido = 2;
+  const byte motor = 5;
+
 //Constantes
 
 //Variables
 byte velocidad;
+bool encendido;
 //  señal recibida por bluetooth
  
 void setup(){
-  //Serial.begin(9600);
-  pinMode(potenciometro, INPUT);
-  pinMode(encendido, INPUT_PULLUP);
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  mySerial.setTimeout(50);
   pinMode(motor, OUTPUT);
 }
 void loop(){
-  //Arranque manual del motor
-  
+  //Bluetooth
+  if (mySerial.available()) {
+    char data[2];
+    size_t count = mySerial.readBytesUntil('\n', data, 2);
+    
+    DEBUG(data, count);
+    if (data == 'ON')
+    {
+      encendido = true;
+    }
+    else if (data == 'OF')
+    {
+      encendido = false;
+    }
+    else
+    {
+      velocidad = (unsigned byte)data[0];
+    }
+  }
   //Se divide entre 4 porque el pwm es de 8 bits (256)
-  velocidad = map(analogRead(potenciometro), 0, 1023, 0, 255);
-  analogWrite(motor, velocidad);
+  if (encendido)
+  {
+    analogWrite(motor, 0);
+  }
+  else
+  {
+    analogWrite(motor, velocidad);
+  }
+  serialFlush();
   delay(50);
-  velocidad = analogRead(potenciometro);
+}
+
+void DEBUG(char a[2], size_t b)
+{
+  if (a[0] == 'O' && a[1] == 'N')
+  {
+    for (int index = 0; index < b; index++)
+      Serial.print(a[index]);
+  }
+  else if (a[0] == 'O' && a[1] == 'F')
+  {
+    for (int index = 0; index < b; index++)
+      Serial.print(a[index]);
+  }
+  else
+  {
+    Serial.print((unsigned byte)a[0]);
+  }
+  Serial.println();
+}
+void serialFlush(){
+  while(mySerial.available() > 0) {
+    char t = mySerial.read();
+  }
 }
