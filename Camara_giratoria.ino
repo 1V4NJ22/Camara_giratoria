c9889eb703a05fd1e1c36a58f1d7fcdfaacb6155
1V4NJ22 <58572213+1V4NJ22@users.noreply.github.com>
2022-04-22 16:49:42 -0700

    Optimizado para el estado de datos recibidos

--- Camara_giratoria.ino
+++ Camara_giratoria.ino
@@ -8,10 +8,10 @@ SoftwareSerial mySerial(A4, A5); //Rx,
 //Constantes
 
 //Variables
-byte velocidad;
-bool encendido;
+  byte velocidad[1];
+  bool encendido;
 //  señal recibida por bluetooth
- 
+
 void setup(){
   Serial.begin(9600);
   mySerial.begin(9600);
@@ -21,54 +21,14 @@ void setup(){
 void loop(){
   //Bluetooth
   if (mySerial.available()) {
-    char data[2];
-    size_t count = mySerial.readBytesUntil('\n', data, 2);
+    mySerial.readBytes(velocidad,1);
+    serialFlush();
     
-    DEBUG(data, count);
-    if (data[0] == 'O' && data[1] == 'N')
-    {
-      encendido = true;
-    }
-    else if (data[0] == 'O' && data[1] == 'F')
-    {
-      encendido = false;
-    }
-    else
-    {
-      velocidad = (byte)data[0];
-    }
+    Serial.println(velocidad[0]);
+    analogWrite(motor, velocidad[0]);
   }
-  //Se divide entre 4 porque el pwm es de 8 bits (256)
-  if (encendido)
-  {
-    analogWrite(motor, velocidad);
-  }
-  else
-  {
-    analogWrite(motor, 0);
-  }
-  serialFlush();
-  delay(50);
 }
-
-void DEBUG(char a[2], size_t b)
-{
-  if (a[0] == 'O' && a[1] == 'N')
-  {
-    for (int index = 0; index < b; index++)
-      Serial.print(a[index]);
-  }
-  else if (a[0] == 'O' && a[1] == 'F')
-  {
-    for (int index = 0; index < b; index++)
-      Serial.print(a[index]);
-  }
-  else
-  {
-    Serial.print((byte)a[0]);
-  }
-  Serial.println();
-}
+// Borrar datos del puerto serial
 void serialFlush(){
   while(mySerial.available() > 0) {
     char t = mySerial.read();
