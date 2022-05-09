ca1f68709858da44e8fa9f9c9cf29f332070f8ee
1V4NJ22 <58572213+1V4NJ22@users.noreply.github.com>
2022-05-09 00:40:53 -0700

    Ahora funciona con 2 bytes

--- Camara_giratoria.ino
+++ Camara_giratoria.ino
@@ -1,4 +1,5 @@
 #include <SoftwareSerial.h>
+//Puertos seriales por definidos por software
 SoftwareSerial mySerial(A4, A5); //Rx,
 //Entradas
 
@@ -7,9 +8,22 @@ SoftwareSerial mySerial(A4, A5); //Rx,
 
 //Constantes
 
+  //número de bytes recibidos por bluetooth a la vez
+  const int bytes = 2;
 //Variables
-  byte velocidad[1];
+
+  //  Cadena de bytes recibidos por el móduilo bluetooth
+  char data[bytes];
+  // Encender motor
   bool encendido;
+  //  Velocidad del motor
+  byte velocidad;
+
+  int tiempoArranque;
+
+  unsigned long tiempoGiro;
+
+  unsigned long timer;
 //  señal recibida por bluetooth
 
 void setup(){
@@ -21,16 +35,48 @@ void setup(){
 void loop(){
   //Bluetooth
   if (mySerial.available()) {
-    mySerial.readBytes(velocidad,1);
+    mySerial.readBytes(velocidad,bytes);
     serialFlush();
+    //2 bytes. el byte 0 es para enviar el tipo de comando y el byte 1 para una cantidad
+    //byte 0
+    //  0: apagar
+    //  1: encender
+    //  2: enviar velocidad
+    //  3: enviar tiempo de arranque
+    switch (data[0])
+    {
+    case 0:
+      encendido = false;
+      break;
     
-    Serial.println(velocidad[0]);
+    case 1:
+      encendido = true;
+      break;
+    
+    case 2:
+      velocidad = data[1];
+      break;
+    
+    case 3:
+      tiempoArranque = data[1];
+      break;
+    
+    case 4:
+      tiempoGiro = data[1];
+      break;
+    }
+
+    Serial.println(velocidad[1]);
     analogWrite(motor, velocidad[0]);
   }
 }
 // Borrar datos del puerto serial
-void serialFlush(){
-  while(mySerial.available() > 0) {
+void serialFlush()
+  while(mySerial.available() > 0)
     char t = mySerial.read();
-  }
+
+void DEBUG(char data[bytes], size_t count){
+  for (int index = 0; index < count; index++)
+    Serial.print(data[index]); 
+  Serial.println();
 }
